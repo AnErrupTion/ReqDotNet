c3eb1cc982c58173059f1209d1207038b1f769bc
ShiningLea <49339966+AnErrupTion@users.noreply.github.com>
2019-10-16 17:54:40 +0200

    Delete ProxyHandler.cs

--- ProxyHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Net;
-
-namespace ReqDotNet
-{
-    public class ProxyHandler
-    {
-        public static IWebProxy ToWebProxy(string proxy) { return new WebProxy("http://" + proxy, false); }
-    }
-}
