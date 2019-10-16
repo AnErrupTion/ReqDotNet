37e8b22a12cbd7d4b5939f8edde0bb8d88d02601
ShiningLea <49339966+AnErrupTion@users.noreply.github.com>
2019-10-16 17:54:55 +0200

    1.2.0

---
+++ Proxy/ProxyHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System;
+
+namespace ReqDotNet
+{
+    public class ProxyHandler
+    {
+        public static IWebProxy ToWebProxy(string proxy)
+        {
+            var p = new WebProxy
+            {
+                Address = new Uri(proxy),
+                BypassProxyOnLocal = false,
+                UseDefaultCredentials = true
+            };
+
+            return p;
+        }
+    }
+}
---
+++ Utils/WebsiteUtils.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ReqDotNet
+{
+    public static class WebsiteUtils
+    {
+
+        public async static Task<string> GetPageSource(string address)
+        {
+            Requests r = new Requests()
+            {
+                Method = HttpMethod.Get,
+                Address = address,
+                UserAgent = UserAgent.Chrome(),
+                Timeout = 5000
+            };
+
+            return await r.SendRequestToString();
+        }
+
+        public async static Task<bool> TestWebsite(string address, int timeout = 10000)
+        {
+            Requests r = new Requests()
+            {
+                Method = HttpMethod.Get,
+                Address = address,
+                UserAgent = UserAgent.Chrome(),
+                Timeout = timeout
+            };
+
+            var response = await r.SendRequest();
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
