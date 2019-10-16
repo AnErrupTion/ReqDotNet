9c33a6d97b4695b8075945076ca79eb6d91fe6e6
ShiningLea <49339966+AnErrupTion@users.noreply.github.com>
2019-10-16 12:56:16 +0200

    1.1.0

--- CookieStorage.cs
+++ CookieStorage.cs
@@ -1,15 +1,14 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace ReqDotNet
 {
     public class CookieStorage
     {
-        private static Dictionary<string, string> tempCookies = new Dictionary<string, string>();
+        private static readonly Dictionary<string, string> tempCookies = new Dictionary<string, string>();
 
-        public static void Add(string name, string value)
-        {
-            tempCookies.Add(name, value);
-        }
+        public static void Add(string name, string value) { tempCookies.Add(name, value); }
+
+        public static bool Contains(string name) { return tempCookies.ContainsKey(name); }
 
         public static string Get(string name)
         {
